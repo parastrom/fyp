0337fff113daec9c73ea0f18cc758a99d2ce5c79
parastrom <adewoletomisin78@gmail.com>
2023-03-16 01:42:33 +0000

    Fixed foreign-key edge

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 import datasets
 import networkx as nx
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 from settings import DATASETS_PATH
 from dataset_loader.spider import Spider
@@ -91,32 +91,60 @@ db_ids, db_indexes = np.unique(test["db_id"], return_index=True)
 
 db_graphs = dict()
 
-for idx, db_id in enumerate(db_ids):
-    db_idx = db_indexes[idx]
+# for idx, db_id in enumerate(db_ids):
+#     db_idx = db_indexes[idx]
+#
+#     columns = test["db_column_names"][db_idx]
+#     table_names = test["db_table_names"][db_idx]
+#     column_types = test["db_column_types"][db_idx]
+#     foreign_keys = test["db_foreign_keys"][db_idx]
+#     primary_keys = test["db_primary_keys"][db_idx]
+#
+#     g = nx.Graph()
+#
+#     for i, sample in enumerate(zip(columns["table_id"][1:], columns["column_name"][1:])):
+#         table_idx = sample[0]
+#         table = table_names[table_idx]
+#         col_name = sample[1].replace(" ", "_")
+#         g.add_node(
+#             i+1, id=f"{table}.{col_name}", name=col_name, table=table,
+#             primary=True if i+1 in primary_keys["column_id"] else False,
+#             type=column_types[i+1]
+#         )
+#
+#     for (n1, n2) in zip(foreign_keys["column_id"], foreign_keys["other_column_id"]):
+#         g.add_edge(n1, n2, foreign=True)
+#
+#     if idx > 2:
+#         break
 
-    columns = test["db_column_names"][db_idx]
-    table_names = test["db_table_names"][db_idx]
-    column_types = test["db_column_types"][db_idx]
-    foreign_keys = test["db_foreign_keys"][db_idx]
-    primary_keys = test["db_primary_keys"][db_idx]
+db_idx = 0
 
-    g = nx.Graph()
+columns = test["db_column_names"][db_idx]
+table_names = test["db_table_names"][db_idx]
+column_types = test["db_column_types"][db_idx]
+foreign_keys = test["db_foreign_keys"][db_idx]
+primary_keys = test["db_primary_keys"][db_idx]
 
-    for i, sample in enumerate(zip(columns["table_id"][1:], columns["column_name"][1:])):
-        table_idx = sample[0]
-        table = table_names[table_idx]
-        col_name = sample[1].replace(" ", "_")
-        g.add_node(
-            i+1, id=f"{table}.{col_name}", name=col_name, table=table,
-            primary=True if i+1 in primary_keys["column_id"] else False,
-            type=column_types[i+1]
-        )
+g = nx.Graph()
+
+for i, sample in enumerate(zip(columns["table_id"][1:], columns["column_name"][1:])):
+    table_idx = sample[0]
+    table = table_names[table_idx]
+    col_name = sample[1].replace(" ", "_")
+    g.add_node(
+        i+1, id=f"{table}.{col_name}", name=col_name, table=table,
+        primary=True if i+1 in primary_keys["column_id"] else False,
+        type=column_types[i+1]
+    )
 
     for (n1, n2) in zip(foreign_keys["column_id"], foreign_keys["other_column_id"]):
-        g.add_edge(n1, n2, foreign=True)
+        g.add_edge(n1-1 , n2-1, foreign=True)
 
-    if idx > 2:
-        break
+plt.figure(figsize=(10, 10), dpi=150)
+nx.draw(g, node_size=60, with_labels=True,)
+plt.savefig("Graph.png", format="PNG")
+plt.show()
 # counter = 0
 # for i, sample in enumerate(cols):
 #     db_id = test["db_id"][i]
